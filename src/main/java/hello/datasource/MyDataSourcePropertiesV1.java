8513e7b6832a7dc119676e39a1caa005b397b3a3
sunwooKim <frod5@kakao.com>
2023-04-30 19:21:01 +0900

    스프링 부트 - 핵심 원리와 활용 : 외부설정 @ConfigurationProperties 사용 실습

---
+++ src/main/java/hello/datasource/MyDataSourcePropertiesV1.java
@@ -0,0 +1,24 @@
+package hello.datasource;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@ConfigurationProperties("my.datasource")  //@ConfigurationProperties 이 있으면 외부 설정을 주입 받는 객체라는 뜻이다
+public class MyDataSourcePropertiesV1 {
+    private String url;
+    private String username;
+    private String password;
+    private Etc etc = new Etc();
+
+    @Data
+    public static class Etc {
+        private int maxConnection;
+        private Duration timeout;
+        private List<String> options = new ArrayList<>();
+    }
+}
